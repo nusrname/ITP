7b033ef379523b667955a891968e065d4a0f0097
nusrname <zhelezniyche@yandex.ru>
2025-04-02 20:05:08 +0400

    refactore: rewrite sorting to change values​instead of pointers

--- 6_3.cpp
+++ 6_3.cpp
@@ -15,6 +15,50 @@ private:
     Node* head;
     Node* tail;
 
+    //void Swap(Node* a, Node* b)
+    //{
+    //    if (!a || !b || a == b) return;
+
+    //    if (a->next == b)
+    //    {                                            // a и b - соседние
+    //        a->next = b->next;
+    //        b->prev = a->prev;
+    //        if (b->next) b->next->prev = a;
+    //        if (a->prev) a->prev->next = b;
+    //        b->next = a;
+    //        a->prev = b;
+    //    }
+    //    else if (b->next == a) 
+    //    {                                           // b и a - соседние (переставлены местами)
+    //        b->next = a->next;
+    //        a->prev = b->prev;
+    //        if (a->next) a->next->prev = b;
+    //        if (b->prev) b->prev->next = a;
+    //        a->next = b;
+    //        b->prev = a;
+    //    }
+    //    else 
+    //    {                                           // не соседние узлы
+    //        Node* aPrev = a->prev;
+    //        Node* aNext = a->next;
+    //        Node* bPrev = b->prev;
+    //        Node* bNext = b->next;
+
+    //        if (aPrev) aPrev->next = b;
+    //        if (aNext) aNext->prev = b;
+    //        if (bPrev) bPrev->next = a;
+    //        if (bNext) bNext->prev = a;
+
+    //        std::swap(a->prev, b->prev);
+    //        std::swap(a->next, b->next);
+    //    }
+
+    //    if (!a->prev) head = a;
+    //    if (!b->prev) head = b;
+    //    if (!a->next) tail = a;
+    //    if (!b->next) tail = b;
+    //}
+
 public:
     List() : head(nullptr), tail(nullptr) {}
 
@@ -53,17 +97,63 @@ public:
         }
         cout << endl;
     }
+
+    Node* partition(Node* low, Node* high) 
+    {
+        if (!low || !high || low == high) return low;
+
+        int pivot = high->data;
+        Node* i = low->prev;
+
+        for (Node* j = low; j != high; j = j->next) 
+        {
+            if (j->data < pivot)
+            {
+                (i == nullptr) ? i = low : i = i->next;
+                /*Swap(i, j);*/
+                swap(i->data, j->data);
+            }
+        }
+
+        (i == nullptr) ? i = low : i = i->next;
+        /*Swap(i, high);*/
+        swap(i->data, high->data);
+        return i;
+    }
+
+
+    void quickSort(Node* low, Node* high) 
+    {
+        if (!low || !high || low == high || low == high->next) return;
+
+        Node* pivot = partition(low, high);
+        if (pivot && pivot->prev) 
+            quickSort(low, pivot->prev);
+        if (pivot && pivot->next) 
+            quickSort(pivot->next, high);
+    }
+
+    Node* GetHead() { return head; }
+
+    Node* GetTail() { return tail; }
+
 };
 
-int main() {
+int main()
+{
     List list;
     int value;
 
-    cout << "Input elements (to stop input press '!'): ";
-    while (cin >> value && value != '!')
+    cout << "Input elements (to stop input non-number): ";
+    while (cin >> value)
         list.append(value);
 
-    list.removeDuplicates();
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    //list.print();
+
+    list.quickSort(list.GetHead(), list.GetTail());
 
     cout << "Result: ";
     list.print();

4d734ad8a72cffefeeb88cc62cedb7c4e29f6d98
nusrname <zhelezniyche@yandex.ru>
2025-04-21 19:15:08 +0400

    feat: implement graph inheritance with directed and undirected graphs, fix edge direction bug

--- 9.cpp
+++ 9.cpp
@@ -5,128 +5,169 @@ using namespace std;
 
 class Graph
 {
-    int V; // количество вершин
-    vector<vector<int>> adj; // список смежности
+protected:
+
+	int V; // количество вершин
+	vector<vector<int>> adj; // список смежности
 
 public:
-    Graph(int V) : V(V), adj(V) {}
 
-    void addEdge(int u, int v)
-    {
-        // Добавление ребра для неориентированного графа
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+	Graph(int V) : V(V), adj(V) {}
 
-    // Вывести количество вершин, смежных с данной
-    int countAdjacentUndirected(int v) 
-    {
-        return adj[v].size();
-    }
+	void print()
+	{
+		cout << "Adjacency List (vertex: neighbors): " << endl;
+		for (size_t i = 0; i < adj.size(); ++i)
+		{
+			cout << i << ": ";
+			for (int neighbor : adj[i])
+				cout << neighbor << ' ';
+			cout << '\n';
+		}
+	}
 
-    // Подсчитать степень каждой вершины
-    vector<int> vertexDegrees() 
-    {
-        vector<int> degrees(V);
-        for (int i = 0; i < V; ++i)
-            degrees[i] = adj[i].size();
-        return degrees;
-    }
+	int getVertexCount() const { return V; }
+};
 
-    // Проверить связность
-    bool isConnected() 
-    {
-        vector<bool> visited(V, false);
-        dfs(0, visited);
-        for (bool v : visited)
-            if (!v) return false;
-        return true;
-    }
+class orderedGraph : protected Graph
+{
+public:
+	orderedGraph(int V) : Graph(V) {}
 
-    void dfs(int v, vector<bool>& visited)
-    {
-        visited[v] = true;
-        for (int u : adj[v])
-            if (!visited[u])
-                dfs(u, visited);
-    }
+	void addEdge(int u, int v)
+	{
+		for (int i = 0; i < adj[v].size(); ++i)
+			if (adj[v][i] == u)
+				return;
+		adj[u].push_back(v);
+	}
 
-    // Вывести все истоки графа
-    vector<int> sourcesDirected() 
-    {
-        vector<int> inDegree(V, 0);
-        for (int u = 0; u < V; ++u)
-            for (int v : adj[u])
-                ++inDegree[v];
+	// Вывести все истоки графа
+	vector<int> sourcesDirected()
+	{
+		vector<int> inDegree(V, 0);
+		for (int u = 0; u < V; ++u)
+			for (int v : adj[u])
+				++inDegree[v];
 
-        vector<int> sources;
-        for (int i = 0; i < V; ++i)
-            if (inDegree[i] == 0)
-                sources.push_back(i);
-        return sources;
-    }
+		vector<int> sources;
+		for (int i = 0; i < V; ++i)
+			if (inDegree[i] == 0)
+				sources.push_back(i);
+		return sources;
+	}
 
-    void print()
-    {
-        cout << "Adjacency List (vertex: neighbors): " << endl;
-        for (size_t i = 0; i < adj.size(); ++i) 
-        {
-            cout << i << ": ";
-            for (int neighbor : adj[i]) 
-                cout << neighbor << ' ';
-            cout << '\n';
-        }
-    }
+	void print() { Graph::print(); }
 
+};
+
+class notOrderedGraph : protected Graph
+{
+public:
+
+	notOrderedGraph(int V) : Graph(V) {}
+
+	void addEdge(int u, int v)
+	{
+		for (int i = 0; i < adj[v].size(); ++i)
+			if (adj[v][i] == u)
+				return;
+		adj[v].push_back(u);
+		adj[u].push_back(v);
+	}
+
+	// Вывести количество вершин, смежных с данной
+	int countAdjacentUndirected(int v)
+	{
+		return adj[v].size();
+	}
+
+	// Подсчитать степень каждой вершины
+	vector<int> vertexDegrees()
+	{
+		vector<int> degrees(V);
+		for (int i = 0; i < V; ++i)
+			degrees[i] = adj[i].size();
+		return degrees;
+	}
+
+	// Проверить связность
+	bool isConnected()
+	{
+		vector<bool> visited(V, false);
+		dfs(0, visited);
+		for (bool v : visited)
+			if (!v) return false;
+		return true;
+	}
+
+	void dfs(int v, vector<bool>& visited)
+	{
+		visited[v] = true;
+		for (int u : adj[v])
+			if (!visited[u])
+				dfs(u, visited);
+	}
+
+	void print() { Graph::print(); }
 };
 
 int main()
 {
-    int V;
-    string t;
-    cout << "Input count of vertices: ";
-    cin >> V;
-    Graph g(V);
-    cout << "Input elements: ";
+	int V;
+	string t;
+	cout << "Input count of vertices: ";
+	cin >> V;
+	notOrderedGraph g(V);
+	cout << "Input adjacency list: " << endl;
+	cin.ignore();
 
-/*        
-        1 2 4 5
-        0 3
-        0 5 
-        1
-        0 6
-        0 2 6 
-        4 5 
-*/
+	for (int i = 0; i < V; ++i)
+	{
+		getline(cin, t);
+		istringstream iss(t);
+		int v;
+		while (iss >> v)
+			g.addEdge(i, v);
+	}
 
-    for (int i = 0; i < V; ++i)
-    {
-        cin >> t;
-        istringstream iss(t);
-        int v;
-        while (iss >> v)
-            g.addEdge(i, v);
-    }
-    g.print();
+	cout << "Input vertex: ";
+	int vertex;
+	cin >> vertex;
+	cout << "Count of adjacent vertices to " << vertex << ": " << g.countAdjacentUndirected(vertex) << endl;
 
-    int vertex = 0;
-    cout << "Count of adjacent vertices to " << vertex << ": " << g.countAdjacentUndirected(vertex) << endl;
+	cout << "Input A and B: ";
+	int A, B;
+	cin >> A >> B;
+	g.addEdge(A, B);
+	cout << "Edge inserted between " << A << " and " << B << endl;
+	g.print();
 
-    g.addEdge(3, 6);
-    cout << "Edge inserted between 3 and 6." << endl;
-    g.print();
+	vector<int> degrees = g.vertexDegrees();
+	cout << "Degrees of vertices: ";
+	for (int d : degrees) cout << d << ' ';
+	cout << endl;
 
-    vector<int> degrees = g.vertexDegrees();
-    cout << "Degrees of vertices: ";
-    for (int d : degrees) cout << d << ' ';
-    cout << endl;
+	cout << "Graph is " << (g.isConnected() ? "connected" : "not connected") << endl << endl;
+	cin.ignore();
 
-    cout << "Graph is " << (g.isConnected() ? "connected" : "not connected") << endl;
+	cout << "Work with ordered graph\nInput adjacency list:" << endl;
 
-    vector<int> sources = g.sourcesDirected();
-    cout << "Sources in directed graph: ";
-    for (int s : sources) cout << s << ' ';
-    cout << endl;
+	orderedGraph dg(V);
+	for (int i = 0; i < V; ++i) 
+	{
+		getline(cin, t);
+		istringstream iss(t);
+		int v;
+		while (iss >> v)
+			dg.addEdge(i, v);
+	}
+	dg.print();
+	vector<int> sources = dg.sourcesDirected();
+	cout << "Sources in directed graph: ";
+	for (int s : sources)
+		cout << s << ' ';
+	cout << endl;
 
-    return 0;
+	return 0;
 }
